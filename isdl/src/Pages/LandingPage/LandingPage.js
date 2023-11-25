57d17c3708cad61c68aa64430e16658314e12b99
Pranjal-2310 <pranjalshp2310@gmail.com>
2023-11-25 11:48:59 +0530

    some updated code for landing page

--- isdl/src/Pages/LandingPage/LandingPage.js
+++ isdl/src/Pages/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Lp from './LandingPage.module.css'
+import lpcss from './LandingPage.module.css'
 // import logo_lnm from "../public/logo_lnm.png";
 // import CourseCard from '../../components/CourseCard/CourseCard';
 // import course from '../../Data/CourseDetails';
@@ -7,9 +7,17 @@ function App()
 {
   return (
 <>
-   {/* <div className={LandingNav}>
-      <img src={logo_lnm}/>
-   </div> */}
+  <div className={lpcss.main}>
+    <div className={lpcss.navbar}>
+      <div className={lpcss.lnmlogo}></div>
+      <div className={lpcss.develop}>Developed By</div>
+    </div>
+    <h1 className={lpcss.heading}>Student Details</h1>
+    <div className={lpcss.container1}>
+      <div className={lpcss.content}>content</div>
+      <div className={lpcss.image}></div>
+    </div>
+  </div>
 
 
 </>
